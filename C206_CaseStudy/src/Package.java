60604b946e76ba62abc0298290b1f7f37361d490
congl <congl@10.174.113.3>
2022-07-28 16:55:31 +0800

    CL did package class

---
+++ C206_CaseStudy/src/Package.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate;
+
+public class Package {
+	
+	private String packageCode;
+	private String packageDes;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private double packageAmount;
+	
+	public Package(String packageCode, String packageDes, LocalDate startDate, LocalDate endDate, double packageAmount) {
+		this.packageCode = packageCode;
+		this.packageDes = packageDes;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.packageAmount = packageAmount;
+	}
+	
+	public String getPackageCode() {
+		return packageCode;
+	}
+	
+	public String getPackageDes() {
+		return packageDes;
+	}
+	
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+	
+	public double getPackageAmount() {
+		return packageAmount;
+	}
+
+}
